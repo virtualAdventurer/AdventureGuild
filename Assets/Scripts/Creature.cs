f3f8b3a8fcfb7ab815251fe7971987f90e0c2091
virtualAdventurer <peter410berry@gmail.com>
2021-04-19 16:08:01 -0700

    Breadth First Search Returns a list of all possible move spaces.
    Each tile is currently sent to log for testing. A bug was fixed that added the first space twice.

--- Assets/Scripts/Creature.cs
+++ Assets/Scripts/Creature.cs
@@ -10,13 +10,19 @@ public class Creature : MonoBehaviour
     public int Move;
 
     private TileData space;
-
+ 
     void Start()
     {
         Vector3 point = transform.position;
         var worldPoint = new Vector3Int(Mathf.FloorToInt(point.x), Mathf.FloorToInt(point.y), 0);
         space = get_data(worldPoint);
-        breadth_first_search(worldPoint);
+        var test = breadth_first_search(worldPoint);
+
+        Debug.Log("Total Number of Spaces: " + test.Count);
+        foreach ((TileData, int) i_space in test)
+        {
+            Debug.Log(i_space.Item1.LocalPlace.x + ", " + i_space.Item1.LocalPlace.y);
+        }
 
         //var up = new Vector3Int(worldPoint.x, worldPoint.y + 1, 0);
         //var left = new Vector3Int(worldPoint.x - 1, worldPoint.y, 0);
@@ -49,11 +55,12 @@ public class Creature : MonoBehaviour
     }
 
 
-    void breadth_first_search(Vector3Int point)
+    List<(TileData, int)> breadth_first_search(Vector3Int point)
     {
         Queue<(TileData, int)> need_to_visit = new Queue<(TileData, int)>();
         List<(TileData, int)> can_reach = new List<(TileData, int)>();
         need_to_visit.Enqueue((space, 0));
+        space.Visited = true;
 
         while(need_to_visit.Count > 0)
         {
@@ -62,7 +69,6 @@ public class Creature : MonoBehaviour
             TileData current_space = temp.Item1;
             int current_cost = temp.Item2;
 
-            can_reach.Add((space, current_cost));
             var up = new Vector3Int(current_space.LocalPlace.x , current_space.LocalPlace.y + 1, 0);
             breadth_add_to_queue(need_to_visit, up, current_cost + 1);
         
@@ -78,13 +84,12 @@ public class Creature : MonoBehaviour
             if(current_cost <= Move)
             {
                 can_reach.Add((current_space, current_cost));
-                //Just for testing
+                //Just for testing, add bittons here?
                 current_space.TilemapMember.SetTileFlags(current_space.LocalPlace, TileFlags.None);
                 current_space.TilemapMember.SetColor(current_space.LocalPlace, Color.green);
             }
         }
-
-        
+        return can_reach;
     }
 
     void breadth_add_to_queue(Queue<(TileData, int)> q, Vector3Int p, int c)
