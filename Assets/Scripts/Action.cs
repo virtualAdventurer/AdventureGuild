2bee07b2e9598da88e8b5981fc83bc9a6c69b50b
virtualAdventurer <peter410berry@gmail.com>
2021-06-17 16:54:41 -0700

    Created new Action Class

---
+++ Assets/Scripts/Action.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Action
+{
+    private string name;
+    public delegate void actionTemplate();
+    private actionTemplate act;
+
+    public Action(string n, actionTemplate func)
+    {
+        name = n;
+        act = func;
+    }
+
+    public void preformAction()
+    {
+        act();
+    }
+}
